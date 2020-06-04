6c345e1f8fc83686fb84054526b7aa1589407729
Aioria <sansil32@gmail.com>
2020-06-04 17:20:24 -0300

    init

---
+++ budgetManager/options.js
@@ -0,0 +1,32 @@
+$(function () {
+
+    chrome.storage.sync.get('limit', function (budget) {
+
+        $('#limit').val(budget.limit)
+    })
+
+    $('#saveLimit').click(function () {
+        var limit = $('#limit').val();
+        if (limit) {
+            chrome.storage.sync.set({
+                'limit': limit
+            }, function () {
+                close();
+            })
+        }
+    })
+
+    $('#resetTotal').click(function () {
+        chrome.storage.sync.set({
+            'total': 0
+        }, function () {
+            var notifOptions = {
+                type: 'basic',
+                iconUrl: 'icon48.png',
+                title: 'Limit reached!',
+                message: 'Uh Oh! lokks like you reached our limit'
+            };
+            chrome.notifications.create('limitNotif', notifOptions)
+        })
+    })
+})
